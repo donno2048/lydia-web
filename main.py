9b753a6430e63b29c8c9a3bbd9f989f934d95217
Elisha Hollander <just4now666666@gmail.com>
2021-03-10 16:57:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-try:import requests,flask;app=flask.Flask(__name__);all_text='';talk=None
+import requests,flask;app=flask.Flask(__name__);all_text='';talk=None
 class API(object):
     def __init__(self, access_key,endpoint="https://api.intellivoid.net/coffeehouse"):
         if isinstance(access_key, API):self.access_key = access_key.access_key;self.endpoint = access_key.endpoint
         else:self.access_key = access_key;self.endpoint = endpoint
-    def _send(self, path, access_key=True, **payload):payload["access_key"] = self.access_key;exec('global final;final='+requests.post("{}/{}".format(self.endpoint, path),payload).text.replace('true','True'));return final["payload"]
+    def _send(self, path, access_key=True, **payload):payload["access_key"] = self.access_key;exec('global final;final='+requests.post("{}/{}".format(self.endpoint, path),payload).text.replace('true','True').replace('false','False'));return final["results"]
 class Session:
     def __init__(self, data, client):self._client = client;self.id = data["session_id"];self.language = data["language"];self.available = data["available"];self.expires = data["expires"]
     def think(self, text):return self._client.think(self.id, text)
@@ -14,7 +14,7 @@ class AI(API):
     def get_session(self, session_id):return Session(self._send("v1/lydia/session/get",session_id=session_id), self)
     def think(self, session_id, text):return self._send("v1/lydia/session/think",session_id=session_id,input=text)["output"]
 @app.route('/')
-def hello():global talk;talk=AI(API(APIKEY)).create_session();return flask.render_template('index.html')
+def hello():global talk;talk=AI(APIKEY).create_session();return flask.render_template('index.html')
 @app.route('/', methods=['POST'])
 def index_post():global all_text;text=flask.request.form['text'];output=talk.think(text);all_text+='You said: '+text+'\n\n'+'Bot said: '+output+'\n\n';return flask.render_template('index.html',text=all_text)
 if __name__ == '__main__':app.run(debug=True)
